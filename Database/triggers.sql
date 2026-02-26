b55648a6dacbbe89e979585dcbafb765a5ce6aaf
shrishabarish <shrishab08@gmail.com>
2026-02-26 22:17:10 +0530

    Updated Triggers commands to prevent user

--- Database/triggers.sql
+++ Database/triggers.sql
@@ -4,3 +4,23 @@ FOR EACH ROW
 BEGIN
     RAISE_APPLICATION_ERROR(-20001, 'Reviews are immutable and cannot be modified.');
 END;
+
+CREATE OR REPLACE TRIGGER trg_prevent_review_update
+BEFORE UPDATE ON REVIEWS
+FOR EACH ROW
+BEGIN
+    RAISE_APPLICATION_ERROR(
+        -20001,
+        'Reviews are immutable and cannot be modified.'
+    );
+END;
+
+CREATE OR REPLACE TRIGGER trg_prevent_review_delete
+BEFORE DELETE ON REVIEWS
+FOR EACH ROW
+BEGIN
+    RAISE_APPLICATION_ERROR(
+        -20002,
+        'Reviews cannot be deleted.'
+    );
+END;
