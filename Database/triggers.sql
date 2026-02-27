eb6a3c03c82b12a3d8d29c3868ee038120cdd1ef
shrishabarish <shrishab08@gmail.com>
2026-02-27 13:33:16 +0530

    Triggers are created

--- Database/triggers.sql
+++ Database/triggers.sql
@@ -4,7 +4,7 @@ FOR EACH ROW
 BEGIN
     RAISE_APPLICATION_ERROR(-20001, 'Reviews are immutable and cannot be modified.');
 END;
-
+/
 CREATE OR REPLACE TRIGGER trg_prevent_review_update
 BEFORE UPDATE ON REVIEWS
 FOR EACH ROW
@@ -14,7 +14,7 @@ BEGIN
         'Reviews are immutable and cannot be modified.'
     );
 END;
-
+/
 CREATE OR REPLACE TRIGGER trg_prevent_review_delete
 BEFORE DELETE ON REVIEWS
 FOR EACH ROW
@@ -24,3 +24,29 @@ BEGIN
         'Reviews cannot be deleted.'
     );
 END;
+/
+
+CREATE OR REPLACE TRIGGER trg_review_audit
+AFTER INSERT ON REVIEWS
+FOR EACH ROW
+BEGIN
+    INSERT INTO AUDIT_LOG (
+        actor_token,
+        action_type,
+        target_id,
+        encrypted_payload,
+        hash_value,
+        previous_hash,
+        log_time
+    )
+    VALUES (
+        :NEW.reviewer_token,
+        'REVIEW_SUBMITTED',
+        :NEW.submission_id,
+        NULL,
+        NULL,
+        NULL,
+        CURRENT_TIMESTAMP
+    );
+END;
+/
