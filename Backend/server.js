57d3d61955676d372ee0bad86bf7137e00463d4c
shrishabarish <shrishab08@gmail.com>
2026-04-04 13:34:38 +0530

    Server code for the Backend

--- Backend/server.js
+++ Backend/server.js
@@ -1,51 +1,31 @@
 require("dotenv").config();
+const connectMongo = require("./db/mongo");
 const express = require("express");
 const cors = require("cors");
-
 const { getConnection } = require("./db/oracle");
 const authRoutes = require("./routes/auth");
-
 const app = express();
 const PORT = process.env.PORT || 5000;
-
-// Middlewares
+connectMongo();
 app.use(cors());
-app.use(express.json());  // Only once — no duplicates
-
-// Test Route
-app.get("/", (req, res) => {
-  res.send("Backend Running 🚀");
-});
-
-// DB Test Route
+app.use(express.json());
+app.get("/", (req, res) => {res.send("Backend is Running ");});
 app.get("/test-db", async (req, res) => {
   try {
     const connection = await getConnection();
-
-    const result = await connection.execute(
-      `SELECT 'Oracle Connected Successfully' AS MESSAGE FROM dual`
-    );
-
+    const result = await connection.execute(`SELECT 'Oracle Connected Successfully' AS MESSAGE FROM dual`);
     await connection.close();
-
     res.json(result.rows);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 });
-
-// Auth Routes
 app.use("/api/auth", authRoutes);
-
 const submissionRoutes = require("./routes/submissions");
 app.use("/api/submissions", submissionRoutes);
-
 const reviewRoutes = require("./routes/reviews");
 app.use("/api/reviews", reviewRoutes);
+const logRoutes = require("./routes/logRoutes");
+app.use("/api/logs", logRoutes);
+app.listen(PORT, () => {console.log(`Server running on port ${PORT}`);});
 
-app.use(express.json());
-
-// Start Server (ALWAYS LAST)
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
